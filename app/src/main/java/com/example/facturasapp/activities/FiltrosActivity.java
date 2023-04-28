6ddb27229061da3ab25b171b797d1d2422b86d6d
6002503 <jorge2002oj@gmail.com>
2023-04-28 14:55:10 +0200

    commit dia 28/04/23

--- app/src/main/java/com/example/facturasapp/activities/FiltrosActivity.java
+++ app/src/main/java/com/example/facturasapp/activities/FiltrosActivity.java
@@ -1,16 +1,52 @@
 package com.example.facturasapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.facturasapp.R;
 
 public class FiltrosActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filtros);
+
+        //Toolbar en blanco
+        toolbar = findViewById(R.id.toolbarPracticaFiltros);
+        FiltrosActivity.this.setSupportActionBar(toolbar);
+
+        //Cambiar toolbar titulo
+        FiltrosActivity.this.setTitle("Filtrar facturas");
+
+        //Boton para salir a la actividad de filtros
+        MenuHost menu = this;
+        menu.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.vuelta_main_app, menu);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.vuelta:
+                        Intent intent = new Intent(FiltrosActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/facturasapp/activities/MainActivity.java
+++ app/src/main/java/com/example/facturasapp/activities/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.facturasapp.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.RelativeLayout;
 
 import com.example.facturasapp.data.adapters.APIAdapter;
@@ -28,12 +35,33 @@ public class MainActivity extends AppCompatActivity implements Callback<FacturaR
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         rv1 = findViewById(R.id.rv1);
-        toolbar = findViewById(R.id.toolbarPractica);
 
+        //Toolbar en blanco
+        toolbar = findViewById(R.id.toolbarPractica);
         MainActivity.this.setSupportActionBar(toolbar);
 
         //Cambiar toolbar titulo
         MainActivity.this.setTitle("Facturas");
+
+        //Boton para ir a la actividad de filtros
+        MenuHost menu = this;
+        menu.addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menuInflater.inflate(R.menu.ida_filtros_app, menu);
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.ida:
+                        Intent intent = new Intent(MainActivity.this, FiltrosActivity.class);
+                        startActivity(intent);
+                        return true;
+                }
+                return false;
+            }
+        });
 
         Call<FacturaResult> call = APIAdapter.getService().getList();
         call.enqueue(this);
